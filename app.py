41016c0af5c16d9b2019be1430a41544d7446b4c
Joshua Herman <zitterbewegung@gmail.com>
2023-06-16 23:27:25 -0500

    Update app.py for queue

--- app.py
+++ app.py
@@ -3,17 +3,14 @@ from twilio.twiml.messaging_response import MessagingResponse
 from inference import query_agent
 from redis import Redis
 from rq import Queue
-from sms import handle_received_sms
-
+from msg import send_text_message
 import os, time, socket, sys, ipaddress
 from collections import deque
 from typing import Dict, List, Optional, Any
-# from agent import BabyAG
 
-# All Context / API managers
+# All Context / API managers                                                                                                                                                                                                                                                   
 app = Flask(__name__)
-
-# Set up RQ
+#celery_app = celery_init_app(app)                                                                                                                                                                                                                                             
 redis_conn = Redis()
 q = Queue(connection=redis_conn)
 
@@ -26,35 +23,44 @@ def split_string(input_string, max_length=320):
 
     return strings_list
 
+
 @app.route("/sms", methods=["POST"])
 def chatgpt():
     """get incoming message"""
-    inb_msg           = request.form["Body"]  # .lower()
+    inb_msg           = request.form["Body"]  # .lower()                                                                                                                                                                                                                       
     to_phone_number   = request.form["To"]
     from_phone_number = request.form["From"]
-    
-    print(inb_msg)
-    print(to_phone_number)
-    print(from_phone_number)
-    q.enqueue(handle_received_sms,
-              inb_msg,
-              from_phone_number,
-              to_phone_number,)  
 
-    #response = "Test" #agent_chain.run(input=inb_msg)
-    #response  = query_agent(inb_msg)
-    
+    print("Inb_msg {}".format(inb_msg))
+    print("Req To phone number {}".format(to_phone_number))
+    print("Req from phone number {}".format(from_phone_number))
+    #result = handle_received_sms.delay(inb_msg, from_phone_number, to_phone_number)                                                                                                                                                                                           
+    # Call function to process the SMS (e.g. process_sms) using Celery                                                                                                                                                                                                         
+    #process_sms.apply_async(args=[message_body])                                                                                                                                                                                                                              
+
+    #with Connection(redis.from_url(redis://localhost:6379/0)):                                                                                                                                                                                                                
+    #    q = Queue()                                                                                                                                                                                                                                                           
+    #    task =                                                                                                                                                                                                                                                                
+    q.enqueue(send_text_message,
+              to_phone_number,
+              from_phone_number,
+              inb_msg)
+
+    response = "Test" #agent_chain.run(input=inb_msg)                                                                                                                                                                                                                          
+    #response  = query_agent(inb_msg)                                                                                                                                                                                                                                          
+
     """Respond to incoming calls with a simple text message."""
-    # Start our TwiML response
-    # time.sleep(2)
+    # Start our TwiML response                                                                                                                                                                                                                                                 
+    # time.sleep(2)                                                                                                                                                                                                                                                            
     resp = MessagingResponse()
-    # Add a message
-    #for msg in split_string(response):
-    #    resp.message(msg)
-    #print(response)
+    # Add a message                                                                                                                                                                                                                                                            
+    for msg in split_string(response):
+        resp.message(msg)
+    #print(response)                                                                                                                                                                                                                                                           
 
     return str(resp)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
