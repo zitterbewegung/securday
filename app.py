1b075348cee48452428707bc6bd576ed228eeec5
Joshua Jay Herman <zitterbewegung@gmail.com>
2023-07-25 19:44:57 -0500

    Adding opt out support

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
+import os, time, socket, sys, ipaddress, logging, json
+from collections import deque
+from typing import Dict, List, Optional, Any
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from inference import query_agent
-import json
 import pika
-import os, time, socket, sys, ipaddress, logging
-from collections import deque
-from typing import Dict, List, Optional, Any
 
 # All Context / API managers
 app = Flask(__name__)
 
+
+# def get_if_opt_out(phone_number):
+#     with open('opt_out.csv', newline='') as csvfile:
+#         opt_outreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+#         for row in opt_outreader:
+#            if row[0] == phone_number and row[1] == "TRUE":
+#                return True
+#            else:
+#                return False
+# def set_opt_in(phone_number):
+#     if not get_if_out(phone_number):
+#         row_to_make = '"{}", "TRUE", "10", "2023-01-01 01:01:01.000", "2023-12-31 01:01:01.000"'
+
+#         with open('opt_out.csv', 'w', newline='') as csvfile:
+#             opt_outwriter = csv.writer(csvfile, delimiter=',',
+#                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#             opt_out.writerow()
+
+
+#def initialize_users()'"+13316255728", "TRUE", "10", "2023-01-01 01:01:01.000", "2023-12-31 01:01:01.000'
+#'"+13316255728", "TRUE", "10", "2023-01-01 01:01:01.000", "2023-12-31 01:01:01.000"''"+13316255728", "TRUE", "10", "2023-01-01 01:01:01.000", "2023-12-31 01:01:01.000"'
 
 def split_string(input_string, max_length=320):
     strings_list = []
@@ -45,14 +65,29 @@ def chatgpt():
     inb_msg = request.form["Body"]  # .lower()
     to_phone_number = request.form["To"]
     from_phone_number = request.form["From"]
+    opt_out_filename = "opt_out.json"
 
+    opt_in = {}
+    with open(opt_out_filename) as json_file:
+        opt_in = json.load(json_file)
+ 
     inbound_request = {}
     inbound_request["inb_msg"] = inb_msg
     inbound_request["To"] = to_phone_number
     inbound_request["From"] = from_phone_number
-
+    
     inbound_request_payload = str(json.dumps(inbound_request))
-    send_to_queue(inbound_request_payload)
+
+    if opt_in.get(from_phone_number): 
+       send_to_queue(inbound_request_payload)
+    else:
+        opt_in[from_phone_number] = True
+    
+    if "STOP" in inb_msg:
+        opt_in[from_phone_number] = False
+
+    with open('opt_out_data.json', 'w') as outfile:
+        json.dump(user, outfile)
 
     logging.info("Inb_msg {}".format(inb_msg))
     logging.info("Req To phone number {}".format(to_phone_number))
