e6369c768a4814150f0cfd05b8ade387fedcbae2
zitterbewegung <huger.neo-0b@icloud.com>
2023-08-06 10:53:45 -0500

    Using waitress now.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from typing import Dict, List, Optional, Any, LiteralString
 from collections import deque
 from persistent_dictionary import PersistentDict 
 from flask import Flask, request
+from waitress import serve
 import pika
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
@@ -85,4 +86,5 @@ def main():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    serve(app, host='0.0.0.0', port=5000)
