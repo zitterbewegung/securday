d6e396a75fdebfafb0906966ed58e60fb5192d09
Joshua Jay Herman <zitterbewegung@gmail.com>
2023-09-09 08:48:13 -0500

    Improving app.py with dotenv. Black code reformatting and making webui.

--- persistent_dictionary.py
+++ persistent_dictionary.py
@@ -1,7 +1,8 @@
 import pickle, json, csv, os, shutil
 
+
 class PersistentDict(dict):
-    ''' Persistent dictionary with an API compatible with shelve and anydbm.
+    """Persistent dictionary with an API compatible with shelve and anydbm.
 
     The dict is kept in memory, so the dictionary operations run as fast as
     a regular dictionary.
@@ -12,26 +13,26 @@ class PersistentDict(dict):
     Output file format is selectable between pickle, json, and csv.
     All three serialization formats are backed by fast C implementations.
 
-    '''
+    """
 
-    def __init__(self, filename, flag='c', mode=None, format='pickle', *args, **kwds):
-        self.flag = flag                    # r=readonly, c=create, or n=new
-        self.mode = mode                    # None or an octal triple like 0644
-        self.format = format                # 'csv', 'json', or 'pickle'
+    def __init__(self, filename, flag="c", mode=None, format="pickle", *args, **kwds):
+        self.flag = flag  # r=readonly, c=create, or n=new
+        self.mode = mode  # None or an octal triple like 0644
+        self.format = format  # 'csv', 'json', or 'pickle'
         self.filename = filename
-        if flag != 'n' and os.access(filename, os.R_OK):
-            fileobj = open(filename, 'rb' if format=='pickle' else 'r')
+        if flag != "n" and os.access(filename, os.R_OK):
+            fileobj = open(filename, "rb" if format == "pickle" else "r")
             with fileobj:
                 self.load(fileobj)
         dict.__init__(self, *args, **kwds)
 
     def sync(self):
-        'Write dict to disk'
-        if self.flag == 'r':
+        "Write dict to disk"
+        if self.flag == "r":
             return
         filename = self.filename
-        tempname = filename + '.tmp'
-        fileobj = open(tempname, 'wb' if self.format=='pickle' else 'w')
+        tempname = filename + ".tmp"
+        fileobj = open(tempname, "wb" if self.format == "pickle" else "w")
         try:
             self.dump(fileobj)
         except Exception:
@@ -39,7 +40,7 @@ class PersistentDict(dict):
             raise
         finally:
             fileobj.close()
-        shutil.move(tempname, self.filename)    # atomic commit
+        shutil.move(tempname, self.filename)  # atomic commit
         if self.mode is not None:
             os.chmod(self.filename, self.mode)
 
@@ -53,14 +54,14 @@ class PersistentDict(dict):
         self.close()
 
     def dump(self, fileobj):
-        if self.format == 'csv':
+        if self.format == "csv":
             csv.writer(fileobj).writerows(self.items())
-        elif self.format == 'json':
-            json.dump(self, fileobj, separators=(',', ':'))
-        elif self.format == 'pickle':
+        elif self.format == "json":
+            json.dump(self, fileobj, separators=(",", ":"))
+        elif self.format == "pickle":
             pickle.dump(dict(self), fileobj, 2)
         else:
-            raise NotImplementedError('Unknown format: ' + repr(self.format))
+            raise NotImplementedError("Unknown format: " + repr(self.format))
 
     def load(self, fileobj):
         # try formats from most restrictive to least restrictive
@@ -70,20 +71,19 @@ class PersistentDict(dict):
                 return self.update(loader(fileobj))
             except Exception:
                 pass
-        raise ValueError('File not in a supported format')
+        raise ValueError("File not in a supported format")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import random
 
     # Make and use a persistent dictionary
-    with PersistentDict('/tmp/demo.json', 'c', format='json') as d:
-        print(d, 'start')
-        d['abc'] = '123'
-        d['rand'] = random.randrange(10000)
-        print(d, 'updated')
+    with PersistentDict("/tmp/demo.json", "c", format="json") as d:
+        print(d, "start")
+        d["abc"] = "123"
+        d["rand"] = random.randrange(10000)
+        print(d, "updated")
 
     # Show what the file looks like on disk
-    with open('/tmp/demo.json', 'rb') as f:
+    with open("/tmp/demo.json", "rb") as f:
         print(f.read())
--- pub.py
+++ pub.py
@@ -2,13 +2,17 @@ import os
 import redis
 import sys
 import json
- 
-redis_conn = redis.Redis(charset="utf-8", decode_responses=True)
- 
-def pub():
 
-    data = {"message": "hello", "from": os.environ.get("TWILIO_PHONE_NUMBER"), "to": "YOUR_NUMBER"}
-    #conversation = client.conversations \
+redis_conn = redis.Redis(charset="utf-8", decode_responses=True)
+
+
+def pub():
+    data = {
+        "message": "hello",
+        "from": os.environ.get("TWILIO_PHONE_NUMBER"),
+        "to": "YOUR_NUMBER",
+    }
+    # conversation = client.conversations \
     #                             .v1 \
     #                             .conversations('CHXK') \
     #                             .fetch()
