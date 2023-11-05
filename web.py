7358bc2fcb4ddc14f360523dc6ed99bd20dc9a9b
Joshua Jay Herman <30265+zitterbewegung@users.noreply.github.com>
2023-11-05 11:18:33 -0600

    Updated langchain to newest version and langchain experimental now houses certain inference

--- web.py
+++ web.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from inference import agent_chain
 
-st.header("Tagline Generation App")
+st.header("Securday: Natural Language Network Scanner")
 st.text("(Powered by GPT-3 Model)")
-
+st.text("Try: Does defcon.org use HTTP/2?")
+st.text("Or what ports are open on 1.1.1.1?")
 
 prompt = st.text_area("Enter your prompt", "", height=5)
 trigger = st.button("Generate")
